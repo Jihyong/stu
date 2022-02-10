5805930de28f16fa6611fd3baef346df04422663
이지형 <jihyong94@naver.com>
2022-02-10 22:55:38 +0900

    파이썬 퀴즈: names 리스트에 있는 각각의 유튜버들을 정해진 메일 형식에 맞게 txt파일로 개별 출력.

--- Quiz.py
+++ Quiz.py
@@ -14,3 +14,15 @@ for name in names:
 
 - 나코 드림.
 ''')
+
+for name in names:
+    with open("{}.txt".format(name),"r",encoding = "utf-8") as email_file:
+        contents = (f"안녕하세요? {name}님.\n\n"   # 문장을 코드 인덴테이션에 맞게 정갈하게 정리하기
+                    "(주)나도출판 편집자 나코입니다.\n"
+                    "현재 저희 출판사는 파이썬에 관한 주제로 책 출간을 기획 중입니다.\n"
+                    f"{name}님의 유튜브 영상을 보고 연락을 드리게 되었습니다.\n"
+                    "자세한 내용은 첨부드리는 제안서를 확인 부탁드리며, 긍정적인 회신 기다리겠습니다.\n"
+                    "좋은 하루 보내세요 ^^\n\n"
+                    "감사합니다.\n"
+                    "- 나코 드림.\n")
+        email_file.write(contents)
