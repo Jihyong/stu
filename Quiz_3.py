106762fd22dd3a3ec9d68067fc460cd866321fe7
이지형 <jihyong94@naver.com>
2022-02-11 23:46:49 +0900

    1. 홀수 표 예매 2. 신조어 퀴즈 맞추기 (클래스 활용) 3. 함수 비교

--- Quiz_3.py
+++ Quiz_3.py
@@ -39,4 +39,15 @@ three()>five()>seven()
 # 따라서       이 부분은 False 이기 때문에 and 뒷 부분은 출력 대상이 아님
 #             그러므로 함수 실행시 three five 만 출력
 
+# 같은 예제) 채용 프로세스
+def 서류심사(): print("1.서류심사"); return True # 한 줄로 쓸 때 ; 로 표시
+def 인적성(): print("2.인적성"); return True
+def 코딩테스트(): print("3.코딩테스트"); return True
+def 면접1(): print("4.면접1"); return True
+def 면접2(): print("5.면접2"); return True
+def 건강검진(): print("6.건강검진"); return True
 
+if 서류심사() and 인적성() and 코딩테스트() and 면접1() and 면접2() and 건강검진():
+    print("최종 합격입니다.")
+else:
+    print("아쉽게도 탈락입니다")
