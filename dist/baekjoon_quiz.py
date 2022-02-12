c59e3cf7aba3ce483f01a6e41ccef0714db91730
이지형 <jihyong94@naver.com>
2022-02-12 23:04:31 +0900

    백준 단계별 문제풀이(while)

--- dist/baekjoon_quiz.py
+++ dist/baekjoon_quiz.py
@@ -1,6 +1,15 @@
+#10952번
 import sys
 while True:
     a , b = map(int,sys.stdin.readline().split())
     if a == 0 and b == 0:
         break
     print(a + b)
+
+#10951  # EOF (End of file 에 대한 예외처리 try ~ except~
+while True:
+    try:
+        a,b = map(int,sys.stdin.readline().split())
+        print(a + b)
+    except:
+        break
