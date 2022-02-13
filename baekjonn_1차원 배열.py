52a2c3318243135aa494be9903b33dec6349f880
이지형 <jihyong94@naver.com>
2022-02-14 00:47:39 +0900

    백준 1차원 배열

--- baekjonn_1차원 배열.py
+++ baekjonn_1차원 배열.py
@@ -5,7 +5,19 @@
 # print(m[0],m[-1])
 
 # 최댓값 및 위치 출력
-n = []
-for n in range(9):
-    n.append(int(input()))
-print(max(n), n.index(n) + 1)
+# n = []
+# for i in range(9):
+#     n.append(int(input()))
+# print(max(n), n.index(max(n)) + 1)
+# n = []
+# for i in range(9):
+#     n.append(int(input()))
+# print(max(n), n.index(max(n))+1)
+
+# 세자리 자연수의 곱 a * b * c의 결과값에 나온 각 숫자 개수 구하기
+a = int(input())
+b = int(input())
+c = int(input())
+result = list(str(a*b*c))
+for i in range(10):
+    print(result.count(str(i)))
