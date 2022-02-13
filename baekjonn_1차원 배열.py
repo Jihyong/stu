ed6b7b38389166769257a7717df21c6becb68a1e
이지형 <jihyong94@naver.com>
2022-02-13 23:58:54 +0900

    백준 1차원 배열

--- baekjonn_1차원 배열.py
+++ baekjonn_1차원 배열.py
@@ -1,6 +1,11 @@
 # 최대 최소 출력
-n = int(input())
-m = list(map(int,input().split()))
-m.sort()
-print(m[0],m[-1])
+# n = int(input())
+# m = list(map(int,input().split()))
+# m.sort()
+# print(m[0],m[-1])
 
+# 최댓값 및 위치 출력
+n = []
+for n in range(9):
+    n.append(int(input()))
+print(max(n), n.index(n) + 1)
