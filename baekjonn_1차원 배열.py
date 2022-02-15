2d7cc0fc9bee8eaa5e1c2b1c11cc41f72d28436b
이지형 <jihyong94@naver.com>
2022-02-15 22:46:31 +0900

    백준 1차원 배열

--- baekjonn_1차원 배열.py
+++ baekjonn_1차원 배열.py
@@ -60,15 +60,36 @@
 # print(mean)
 
 # OX 퀴즈 문제( 점수 구하기)
-n = int(input())
-for _ in range(n):
-    ox = input()
-    score = 0
+# n = int(input())
+# for _ in range(n):
+#     ox = input()
+#     score = 0
+#     cnt = 0
+#     for i in range(len(ox)):
+#         if ox[i] == 'O':
+#             cnt += 1
+#             score += cnt
+#         elif ox[i] == 'X':
+#             cnt = 0
+#     print(score)
+
+# n개의 테스트 케이스
+c = int(input())
+avg = []
+for _ in range(c):
+    n = list(map(int,input().split()))
+
+    sum = 0
+    for i in range(n[0]):
+        sum += n[i+1]
     cnt = 0
-    for i in range(len(ox)):
-        if ox[i] == 'O':
+    for i in range(n[0]):
+        if n[i+1] > (sum / n[0]):
             cnt += 1
-            score += cnt
-        elif ox[i] == 'X':
-            cnt = 0
-    print(score)
+    avg.append(cnt / n[0] * 100)
+    n.clear()
+
+for i in range(c):
+    print("%.3f" %avg[i] + "%")
+
+
