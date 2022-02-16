47f619405e8faea01746eb8485e319646a25a89f
이지형 <jihyong94@naver.com>
2022-02-16 23:27:54 +0900

    백준 1차원 배열

--- baekjonn_1차원 배열.py
+++ baekjonn_1차원 배열.py
@@ -74,22 +74,23 @@
 #     print(score)
 
 # n개의 테스트 케이스
-c = int(input())
-avg = []
-for _ in range(c):
-    n = list(map(int,input().split()))
-
-    sum = 0
-    for i in range(n[0]):
-        sum += n[i+1]
-    cnt = 0
-    for i in range(n[0]):
-        if n[i+1] > (sum / n[0]):
-            cnt += 1
-    avg.append(cnt / n[0] * 100)
-    n.clear()
-
-for i in range(c):
-    print("%.3f" %avg[i] + "%")
+# c = int(input())
+# avg = []
+# for _ in range(c):
+#     n = list(map(int,input().split()))
+#
+#     sum = 0
+#     for i in range(n[0]):
+#         sum += n[i+1]
+#     cnt = 0
+#     for i in range(n[0]):
+#         if n[i+1] > (sum / n[0]):
+#             cnt += 1
+#     avg.append(cnt / n[0] * 100)
+#     n.clear()
+#
+# for i in range(c):
+#     print("%.3f" %avg[i] + "%")
+#
 
 
