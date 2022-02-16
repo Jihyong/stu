70af6c534c9f2b8e7d145b2103f36838f83d1505
이지형 <jihyong94@naver.com>
2022-02-17 01:00:51 +0900

    백준 1차원 배열

--- baekjoon_함수.py
+++ baekjoon_함수.py
@@ -11,10 +11,22 @@
 natural_num = set(range(1,10001))
 generated_num = set()
 
-for i in range(1,10001):
-    for j in str(i):
-        i += int(j)
-    generated_num.add(i)
-self_num = sorted(natural_num - generated_num)
-for i in self_num:
-    print(i)
+# for i in range(1,10001):
+#     for j in str(i):
+#         i += int(j)
+#     generated_num.add(i)
+# self_num = sorted(natural_num - generated_num)
+# for i in self_num:
+#     print(i)
+
+# 한수
+n = int(input())
+hansu = 0
+for i in range(1,n+1):
+    if i < 100:
+        hansu += 1
+    else:
+        n_str = list(map(int,str(i)))
+        if n_str[2] - n_str[1] == n_str[1] - n_str[0]:
+            hansu += 1
+print(hansu)
