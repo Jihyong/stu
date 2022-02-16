ac456fad334ce65749f08260534bd87ece689dbe
이지형 <jihyong94@naver.com>
2022-02-16 23:28:05 +0900

    백준 1차원 배열

--- baekjoon_함수.py
+++ baekjoon_함수.py
@@ -1,8 +1,20 @@
-def solve(a):
-    number = 0
-    for i in a:
-        number += i
-        return number
-a = [40,60,90,80]
-result = solve(a)
-print(result)
+# def solve(a):
+#     number = 0
+#     for i in a:
+#         number += i
+#         return number
+# a = [40,60,90,80]
+# result = solve(a)
+# print(result)
+
+# 셀프 넘버
+natural_num = set(range(1,10001))
+generated_num = set()
+
+for i in range(1,10001):
+    for j in str(i):
+        i += int(j)
+    generated_num.add(i)
+self_num = sorted(natural_num - generated_num)
+for i in self_num:
+    print(i)
